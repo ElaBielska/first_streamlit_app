38ffbdcb22a88cc1e3e0cbd47d66fd53e6ea2131
ElaBielska <156688234+ElaBielska@users.noreply.github.com>
2024-01-15 16:31:18 +0100

    Update streamlit_app.py
    add icons

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
+
 streamlit.header('Breakfast Menu')
-streamlit.text('Strawberry Oeatmeal')
-streamlit.text('Spinach smoothee')
-streamlit.text('Boiled eggs')
+
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado toasts')
